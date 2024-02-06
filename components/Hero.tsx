34045094e21deb5f31acc7ac8aac061716fab169
Hariharan1828 <hariharanr.yr@gmail.com>
2024-02-06 13:18:51 +0530

    deployment 3rd resolve

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
               UniVersaLink Solutions
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-            Lorem ipsum is commonly used as placeholder text in design and publishing. Its origins can be traced back a passage from Cicero'sDe Finibus Bon et Malorum
+            Lorem ipsum is commonly used as placeholder text in design and publishing. Its origins can be traced back a passage from CicerosDe Finibus Bon et Malorum
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
